cd5ab9ec09140e932ae3d5132064fac77b98681c
lucasaf <ferreira.lucasalves@gmail.com>
2022-06-08 15:09:53 -0300

    ensure convertAmount throw when getCurrency is throw

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -50,5 +50,14 @@ describe('ExchangeService', () => {
         'BRL',
       );
     });
+
+    it('should be throw when getCurrency is throw', async () => {
+      (currenciesService.getCurrency as jest.Mock).mockRejectedValue(
+        new Error(),
+      );
+      await expect(
+        service.convertAmount({ from: 'INVALID', to: 'BRL', amount: 1 }),
+      ).rejects.toThrow();
+    });
   });
 });
